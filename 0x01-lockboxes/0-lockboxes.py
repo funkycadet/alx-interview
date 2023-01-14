fea83e096f4bcd43194928311cc18944e2c45778
funkycadet <ericsmithsonian2@gmail.com>
2023-01-14 01:34:41 +0100

    task: method to determine open boxes with available keys

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 0-lockboxes module """
+
+
+def canUnlockAll(boxes):
+    """ function to determine if all boxes
+    can be opened. boxes in this case is a
+    list of lists """
+    for i in boxes:
+        for j in i:
+            print(i)
