cb6fd7cb41271dd41098716ff8e3e69fb44f8c5a
funkycadet <ericsmithsonian2@gmail.com>
2023-02-14 11:29:32 +0100

    fix: resolve semistandard issue

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const API_URL = "https://swapi-api.alx-tools.com/api/films";
+const API_URL = 'https://swapi-api.alx-tools.com/api/films';
 
 if (process.argv.length > 2) {
   request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
